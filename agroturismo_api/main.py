8ae0b2033e304f4bbb02caa6712e15a2da39a0ce
Douglas Gusson <douglasgusson@gmail.com>
2023-04-13 17:51:32 -0300

    :rocket: Liberando origens CORS

--- agroturismo_api/main.py
+++ agroturismo_api/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from .core.config import (API_PREFIX, APP_DESCRIPTION, APP_NAME, APP_VERSION,
-                          IS_DEBUG)
+from .core.config import API_PREFIX, APP_DESCRIPTION, APP_NAME, APP_VERSION, IS_DEBUG
 from .core.db import create_db_and_tables, engine
 from .routes import main_router
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://agroturismo.vercel.app",
+]
 
 
 def get_app() -> FastAPI:
@@ -14,6 +20,14 @@ def get_app() -> FastAPI:
         debug=IS_DEBUG,
     )
     fast_app.include_router(main_router, prefix=API_PREFIX)
+
+    fast_app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return fast_app
 
