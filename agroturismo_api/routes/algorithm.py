dc790d0ad95943aa6549589f217eed1fa4c9abef
Douglas Gusson <douglasgusson@gmail.com>
2023-06-04 14:43:41 -0300

    Adição endpoint TSP

--- agroturismo_api/routes/algorithm.py
+++ agroturismo_api/routes/algorithm.py
@@ -3,6 +3,11 @@ from typing import List, Tuple
 from fastapi import APIRouter, HTTPException, Query, status
 from sqlmodel import Session
 
+import numpy as np
+
+from python_tsp.distances import osrm_distance_matrix
+from python_tsp.heuristics import solve_tsp_local_search
+
 from ..core.db import ActiveSession
 from ..models.local import Local, LocalRead
 from ..services.algorithms import guided_local_search, tabu_search
@@ -96,6 +101,50 @@ async def calculate_best_route_tabu_search(
 
     # seleciona os locais ordenados
     locals = session.query(Local).filter(Local.id.in_(locals_ids)).all()
+
+    # Retorna a lista de locais ordenados
+    return locals
+
+@router.get("/tsp", response_model=List[LocalRead])
+async def calculate_tsp_route(
+    *,
+    current_latitude: float,
+    current_longitude: float,
+    ids: List[int] = Query(None),
+    session: Session = ActiveSession
+):
+    """
+    Função que calcula o TSP para uma lista de IDs de locais
+    """
+    if ids is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Locais não informados",
+        )
+
+    start_point = (current_latitude, current_longitude)
+
+    # Obtém as coordenadas dos locais
+    coords = get_coords(ids, session)
+
+    # Adiciona a coordenada atual no início da lista
+    coords.insert(0, start_point)
+    sources = np.array(coords)
+
+    distance_matrix = osrm_distance_matrix(
+        sources, osrm_server_address="https://router.project-osrm.org"
+    )
+
+    permutation, distance = solve_tsp_local_search(distance_matrix)
+
+    permutation = list(map(lambda i: (i - 1), permutation[1:]))
+    locals_ids = [ids[i] for i in permutation]
+
+    # seleciona os locais
+    locals = session.query(Local).filter(Local.id.in_(locals_ids)).all()
+
+    # ordena os locais
+    locals = sorted(locals, key=lambda local: locals_ids.index(local.id))
 
     # Retorna a lista de locais ordenados
     return locals
--- agroturismo_api/utils/calculate_distance_matrix.py
+++ agroturismo_api/utils/calculate_distance_matrix.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+import httpx
 import numpy as np
 
 from .haversine_distance import haversine_distance
@@ -17,3 +18,32 @@ def calculate_distance_matrix(coords: List[Tuple[float, float]]):
             dist_matrix[i][j] = dist
             dist_matrix[j][i] = dist
     return dist_matrix
+
+
+def get_distance_matrix_osrm(points: List[Tuple[float, float]]):
+    """
+    Função que obtém a matriz de distâncias a partir de um conjunto de coordenadas
+    """
+    url = f"https://router.project-osrm.org/table/v1/driving/"
+    params = {
+        "sources": ";".join([f"{i}" for i in range(len(points))]),
+        "destinations": ";".join([f"{i}" for i in range(len(points))]),
+        "annotations": "distance"
+    }
+
+    url_with_points = url + ";".join([f"{p[1]},{p[0]}" for p in points])
+
+    response = httpx.get(url_with_points, params=params)
+
+    distances = response.json()["distances"]
+    matrix = np.zeros((len(points), len(points)))
+
+    for i, row in enumerate(distances):
+        for j, element in enumerate(row):
+            matrix[i][j] = element / 1000  # Dividir por 1000 para obter responsedistâncias em km
+
+    request_url = response.request.url
+
+    print("\n\n", request_url, matrix, sep="\n", end="\n\n")
+
+    return matrix
